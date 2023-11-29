720c3de6ebdbecec672293d8856646a037cd3ded
Simon Duflot <narcisse.duflot@gmail.com>
2023-11-29 15:57:33 +0100

    Redux persist added

--- frontend/App.js
+++ frontend/App.js
@@ -3,9 +3,18 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 
+//////////////////////IMPORT REDUX PERSIST//////////////////
+import { persistStore, persistReducer } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import storage from "redux-persist/lib/storage";
+
 //////////////////////IMPORT REDUX STORE///////////////////
 import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
+import {
+  configureStore,
+  combineReducers,
+  getDefaultMiddleware,
+} from "@reduxjs/toolkit";
 import user from "./reducers/user";
 
 //////////////////////IMPORT NAV//////////////////////////
@@ -13,10 +22,15 @@ import HomeScreen from "./screens/HomeScreen";
 import MapScreen from "./screens/MapScreen";
 import PlacesScreen from "./screens/PlacesScreen";
 
-//////////////////////INIT STORE/////////////////////////
+//////////////////////INIT STORE & PERSIST STORE/////////////////////////
+const reducers = combineReducers({ user });
+const persistConfig = { key: "locate-me-local-01", storage };
 const store = configureStore({
-  reducer: { user },
+  reducer: persistReducer(persistConfig, reducers),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
 });
+const persistor = persistStore(store);
 
 /////////////////////INIT NAV////////////////////////////
 const Stack = createNativeStackNavigator();
@@ -52,12 +66,14 @@ const TabNavigator = () => {
 export default function App() {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="TabNavigator" component={TabNavigator} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <PersistGate persistor={persistor}>
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="TabNavigator" component={TabNavigator} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PersistGate>
     </Provider>
   );
 }
